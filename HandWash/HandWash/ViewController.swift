a5a29ceba13a729281cae9f977655a375753ad4e
Andre Papoti <andrepapoti@gmail.com>
2020-04-07 10:48:33 -0300

    Projeto criado

---
+++ HandWash/HandWash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandWash
+//
+//  Created by André Papoti de Oliveira on 07/04/20.
+//  Copyright © 2020 AndrePapoti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

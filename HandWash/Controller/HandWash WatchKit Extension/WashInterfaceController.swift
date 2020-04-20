7d16cf3d0e9ba03ae4d664c80edefd79251da172
Felipe Semissatto <felipesemi@gmail.com>
2020-04-20 12:56:11 -0300

    Outlets na WashController

---
+++ HandWash/Controller/HandWash WatchKit Extension/WashInterfaceController.swift
@@ -0,0 +1,37 @@
+//
+//  WashInterfaceController.swift
+//  HandWash WatchKit Extension
+//
+//  Created by Felipe Semissatto on 20/04/20.
+//  Copyright © 2020 AndrePapoti. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class WashInterfaceController: WKInterfaceController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var inlineMovie: WKInterfaceInlineMovie!
+    @IBOutlet weak var labelDescription: WKInterfaceLabel!
+    @IBOutlet weak var imageProgress: WKInterfaceImage!
+    
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}

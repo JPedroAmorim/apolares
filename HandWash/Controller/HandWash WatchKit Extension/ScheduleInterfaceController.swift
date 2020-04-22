8916e7e5d74008eb74ba16b7b26fab4a665bb918
Makoto <rmt.makoto@gmail.com>
2020-04-22 12:40:03 -0300

    Notification incompleto

--- HandWash/Controller/HandWash WatchKit Extension/ScheduleInterfaceController.swift
+++ HandWash/Controller/HandWash WatchKit Extension/ScheduleInterfaceController.swift
@@ -8,15 +8,48 @@
 
 import WatchKit
 import Foundation
-
+import UserNotifications
 
 class ScheduleInterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    
+    @IBAction func scheduleAction() {
+        sendMyNotification()
+    
+    }
+    
+    func sendMyNotification(){
+        if #available(watchOSApplicationExtension 3.0, *) {
 
-    @IBOutlet weak var stopwatch: WKInterfaceTimer!
+            let center = UNUserNotificationCenter.current()
+
+            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                // Enable or disable features based on authorization.
+            }
+
+
+            let content = UNMutableNotificationContent()
+            content.title = NSString.localizedUserNotificationString(forKey: "Hello!", arguments: nil)
+            content.body = NSString.localizedUserNotificationString(forKey: "Hello_message_body", arguments: nil)
+            content.sound = UNNotificationSound.default
+            content.categoryIdentifier = "REMINDER_CATEGORY"
+            // Deliver the notification in five seconds.
+            let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5, repeats: false)
+            let id = String(Date().timeIntervalSinceReferenceDate)
+            let request = UNNotificationRequest.init(identifier: id, content: content, trigger: trigger)
+
+            // Schedule the notification.
+
+            center.add(request ,withCompletionHandler: nil)
+
+        } else {
+            // Fallback on earlier versions
+        }
+    }
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         
     }
 

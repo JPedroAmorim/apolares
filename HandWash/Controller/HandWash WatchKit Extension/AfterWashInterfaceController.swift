5d7b4a9aeb147074f294d5e4e23c4b589f69dbb4
Andre Papoti <andrepapoti@gmail.com>
2020-04-23 12:45:21 -0300

    Rough notification

--- HandWash/Controller/HandWash WatchKit Extension/AfterWashInterfaceController.swift
+++ HandWash/Controller/HandWash WatchKit Extension/AfterWashInterfaceController.swift
@@ -8,16 +8,82 @@
 
 import WatchKit
 import Foundation
+import UserNotifications
 
 class AfterWashInterfaceController: WKInterfaceController {
 
     // MARK: - Outlets
     @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var buttonSetAlarm: WKInterfaceButton!
+    @IBOutlet weak var buttonDontRemindMe: WKInterfaceButton!
     
     // MARK: - Variables
     var crownAcumulator: Double = 0
     var numberOfTimeIntervals: Int = 0 // Each interval is equivalent to 15 minutes
     
+    @IBAction func setAlarm() {
+        print(NSDate.now)
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings(completionHandler: { settings in
+            print(settings)
+            if settings.authorizationStatus == .notDetermined {
+                // Ask for user permissions
+                // ### nao tenho certeza sobre as autorizacoes necessarias ainda
+                center.requestAuthorization(options: [.alert, .sound, .badge],
+                                            completionHandler: { granted, error in
+                                                if let error = error {
+                                                    print(error)
+                                                }
+                                                else if granted {
+                                                    self.createNotification(NCenter: center)
+                                                }
+                                                
+                })
+            }
+            else if settings.authorizationStatus == .denied {
+                // Alert the user to change permission
+                // ### Nao sei se devo mandar o usuario pros settings
+                center.requestAuthorization(options: [.alert, .sound, .badge],
+                                            completionHandler: { granted, error in
+                                                if let error = error {
+                                                    print(error)
+                                                }
+                                                else if granted {
+                                                    self.createNotification(NCenter: center)
+                                                }
+                                                
+                })
+            }
+            else { // permission is provisional or authorized
+                // Schedule a notification
+                self.createNotification(NCenter: center)
+            }
+        })
+    }
+    
+    func createNotification(NCenter: UNUserNotificationCenter) {
+        var components = DateComponents()
+        let timerHours: Int = self.numberOfTimeIntervals / 4
+        let timerMinutes: Int = (self.numberOfTimeIntervals % 4) * 15
+        components.calendar = Calendar.current
+        components.hour = timerHours // + current hour
+        components.minute = timerMinutes // + current minute
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components,
+                                                    repeats: false)
+        let content = UNNotificationContent()
+        let request = UNNotificationRequest(identifier: "Test notification",
+                                            content: content,
+                                            trigger: trigger)
+        NCenter.add(request, withCompletionHandler: { (error) in
+            if error != nil {
+                print(error ?? "nao sie")
+            }
+            else {
+                print("notification scheduled")
+            }
+
+        })
+    }
     // MARK: - Lifecycle methods
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -27,10 +93,12 @@ class AfterWashInterfaceController: WKInterfaceController {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         setTitle("Set alarm")
+        // Crown setup
         crownSequencer.delegate = self
         crownSequencer.focus()
+        // Timer setup
         let date = Date(timeIntervalSinceNow: 2 * 60 * 60 + 1) // 2 hours and 1 sec
-        numberOfTimeIntervals = 8
+        numberOfTimeIntervals = 8 // Sync this variable with the time suggested
         timer.setDate(date) // Timer suggestion
     }
 

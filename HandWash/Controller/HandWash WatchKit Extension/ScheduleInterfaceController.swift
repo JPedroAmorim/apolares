710695f4dbe3c5cc64c73fce3b9e9d9b02d9ca6f
Andre Papoti <andrepapoti@gmail.com>
2020-04-24 12:36:01 -0300

    Schedule Interface Controller

--- HandWash/Controller/HandWash WatchKit Extension/ScheduleInterfaceController.swift
+++ HandWash/Controller/HandWash WatchKit Extension/ScheduleInterfaceController.swift
@@ -11,57 +11,129 @@ import Foundation
 import UserNotifications
 
 class ScheduleInterfaceController: WKInterfaceController {
-    
+    // MARK: - Outlets
     @IBOutlet weak var timer: WKInterfaceTimer!
     
     @IBAction func scheduleAction() {
-        sendMyNotification()
-    
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings(completionHandler: { settings in
+            //print(settings)
+            if settings.authorizationStatus == .notDetermined {
+                // Ask for user permissions
+                // ### nao tenho certeza sobre as autorizacoes necessarias ainda
+                center.requestAuthorization(options: [.alert, .sound, .badge],
+                                            completionHandler: { granted, error in
+                                                if let error = error {
+                                                    print(error)
+                                                }
+                                                else if granted {
+                                                    self.createNotification(NCenter: center)
+                                                }
+                                                
+                })
+            }
+            else if settings.authorizationStatus == .denied {
+                // Alert the user to change permission
+                // ### Nao sei se devo mandar o usuario pros settings
+                center.requestAuthorization(options: [.alert, .sound, .badge],
+                                            completionHandler: { granted, error in
+                                                if let error = error {
+                                                    print(error)
+                                                }
+                                                else if granted {
+                                                    self.createNotification(NCenter: center)
+                                                }
+                })
+            }
+            else { // permission is provisional or authorized
+                // Schedule a notification
+                self.createNotification(NCenter: center)
+            }
+        })
     }
     
-    func sendMyNotification(){
-        if #available(watchOSApplicationExtension 3.0, *) {
+    // MARK: - Variables
+    var crownAcumulator: Double = 0
+    var numberOfTimeIntervals: Int = 0 // Each interval is equivalent to 15 minutes
+    
+    // MARK: - Lifecycle
+   override func willActivate() {
+       // This method is called when watch view controller is about to be visible to user
+       super.willActivate()
+       setTitle("Set alarm")
+       // Crown setup
+       crownSequencer.delegate = self
+       crownSequencer.focus()
+       // Timer setup
+       let date = Date(timeIntervalSinceNow: 2 * 60 * 60 + 1) // 2 hours and 1 sec
+       numberOfTimeIntervals = 8 // Sync this variable with the time suggested
+       timer.setDate(date) // Timer suggestion
+   }
 
-            let center = UNUserNotificationCenter.current()
-
-            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
-                // Enable or disable features based on authorization.
+    // MARK: - Methods
+    func createNotification(NCenter: UNUserNotificationCenter) {
+        var components = DateComponents()
+        let date = Date()
+        let timerHours: Int = self.numberOfTimeIntervals / 4
+        let timerMinutes: Int = (self.numberOfTimeIntervals % 4) * 15
+        let calendar = Calendar.current
+        components.calendar = calendar
+        components.hour = timerHours + calendar.component(.hour, from: date)
+        components.minute = timerMinutes + calendar.component(.minute, from: date)
+        // Have to fix these ! later ..
+        if components.minute! > 60 {
+            components.minute! -= 60
+            components.hour! += 1
+        }
+        if let hour = components.hour, let minutes = components.minute {
+            print("Alarm scheduled to \(hour):\(minutes)")
+        }
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components,
+                                                    repeats: false)
+        // let testTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let content = UNMutableNotificationContent()
+        content.categoryIdentifier = "myNotification"
+        content.title = "HandWash"
+        content.body = "Wash hands plz"
+        content.sound = UNNotificationSound.default
+        let request = UNNotificationRequest(identifier: "Test notification",
+                                            content: content,
+                                            trigger: trigger)
+        NCenter.add(request, withCompletionHandler: { (error) in
+            if error != nil {
+                print(error ?? "nao sei")
+            }
+            else {
+                print("notification scheduled")
+                
             }
 
+        })
+    }
+}
 
-            let content = UNMutableNotificationContent()
-            content.title = NSString.localizedUserNotificationString(forKey: "Hello!", arguments: nil)
-            content.body = NSString.localizedUserNotificationString(forKey: "Hello_message_body", arguments: nil)
-            content.sound = UNNotificationSound.default
-            content.categoryIdentifier = "REMINDER_CATEGORY"
-            // Deliver the notification in five seconds.
-            let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5, repeats: false)
-            let id = String(Date().timeIntervalSinceReferenceDate)
-            let request = UNNotificationRequest.init(identifier: id, content: content, trigger: trigger)
-
-            // Schedule the notification.
-
-            center.add(request ,withCompletionHandler: nil)
-
-        } else {
-            // Fallback on earlier versions
+// MARK: - WKCrownDelegate
+extension ScheduleInterfaceController: WKCrownDelegate {
+    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        crownAcumulator += rotationalDelta
+        if crownAcumulator > 1 {
+            if numberOfTimeIntervals < 96  {
+                numberOfTimeIntervals += 1
+                let seconds = numberOfTimeIntervals * 60 * 15 + 1
+                let date: Date = Date(timeIntervalSinceNow: TimeInterval(seconds))
+                timer.setDate(date)
+            }
+            crownAcumulator -= 1
+        }
+        else if crownAcumulator < -1 {
+            if numberOfTimeIntervals > 0 {
+                numberOfTimeIntervals -= 1
+                let seconds = numberOfTimeIntervals * 60 * 15 + 1
+                let date: Date = Date(timeIntervalSinceNow: TimeInterval(seconds))
+                timer.setDate(date)
+            }
+            crownAcumulator += 1
         }
     }
-    
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-        
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
 }

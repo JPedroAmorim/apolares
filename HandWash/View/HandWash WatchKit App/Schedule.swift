fc11b01d3b3d4008baa1bbc3a8258b3b592576c4
Andre Papoti <andrepapoti@gmail.com>
2020-04-27 12:42:31 -0300

    Singleton e adicoes em afterwash e schedule

---
+++ HandWash/View/HandWash WatchKit App/Schedule.swift
@@ -0,0 +1,9 @@
+//
+//  Schedule.swift
+//  HandWash WatchKit Extension
+//
+//  Created by André Papoti de Oliveira on 27/04/20.
+//  Copyright © 2020 AndrePapoti. All rights reserved.
+//
+
+import Foundation

a3febb6e7d7d556a12417a5118b2a915da4bcf87
Makoto <rmt.makoto@gmail.com>
2020-05-11 13:26:52 -0300

    Main tutorial - animateSequence done
    need to implement user default

--- HandWash/Controller/HandWash WatchKit Extension/InterfaceController.swift
+++ HandWash/Controller/HandWash WatchKit Extension/InterfaceController.swift
@@ -17,12 +17,20 @@ class InterfaceController: WKInterfaceController {
     // MARK: - Outlets
     @IBOutlet weak var groupRingProgress: WKInterfaceGroup!
     @IBOutlet weak var labelFraction: WKInterfaceLabel!
+    @IBOutlet weak var labelRingInstruction: WKInterfaceLabel!
+    @IBOutlet weak var labelButtonInstruction: WKInterfaceLabel!
+    @IBOutlet weak var buttonStart: WKInterfaceButton!
+    @IBOutlet weak var buttonSchedule: WKInterfaceButton!
+    
+    // MARK: - Variables
+    var animationTimer: Timer?
+    var stageAnimation = 1
     
     // MARK: - Lifecycle methods
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        
     }
-    
     
     override func willActivate() {
         super.willActivate()
@@ -32,6 +40,8 @@ class InterfaceController: WKInterfaceController {
         self.labelFraction.setText("\(numberOfWashesToday)/5")
         
         self.startAnimationRing(numberOfWashesToday: numberOfWashesToday)
+        
+        self.animateSequence()
     }
     
     override func didDeactivate() {
@@ -50,5 +60,72 @@ class InterfaceController: WKInterfaceController {
                                                         duration: duration,
                                                         repeatCount: 1)
 
+    }
+    
+    private func ringAnimate() {
+        self.animate(withDuration: 1, animations: {
+            self.buttonStart.setAlpha(0.2)
+            self.buttonSchedule.setAlpha(0.2)
+            self.labelRingInstruction.setHidden(false)
+            self.labelRingInstruction.setText("This indicates the progress of the daily goal.")
+        })
+    }
+    
+    private func startButtonAnimate() {
+        self.animate(withDuration: 1, animations: {
+            self.groupRingProgress.setHidden(true)
+            self.labelRingInstruction.setHidden(true)
+            
+            self.buttonSchedule.setAlpha(0.2)
+            self.buttonStart.setAlpha(1)
+            
+            self.labelButtonInstruction.setHidden(false)
+            self.labelButtonInstruction.setText("Start button starts the hand wash process.")
+        })
+    }
+    
+    private func scheduleButtonAnimate() {
+        self.animate(withDuration: 1, animations: {
+            self.buttonStart.setAlpha(0.2)
+            self.buttonSchedule.setAlpha(1)
+            
+            self.labelButtonInstruction.setText("Schedule button edit alarm.")
+        })
+    }
+    
+    private func finishAnimate() {
+        self.animate(withDuration: 1, animations: {
+            self.buttonStart.setAlpha(1)
+            
+            self.groupRingProgress.setHidden(false)
+        
+            self.labelButtonInstruction.setHidden(true)
+        })
+    }
+    
+    private func animateSequence() {
+        self.ringAnimate()
+        
+        self.animationTimer = Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true) { (Timer) in
+            
+            switch self.stageAnimation {
+            case 1:
+                self.startButtonAnimate()
+            case 2:
+                self.scheduleButtonAnimate()
+            case 3:
+                self.finishAnimate()
+            case 4:
+                self.stageAnimation = 0
+                
+                Timer.invalidate()
+                
+            default:
+                Timer.invalidate()
+                print("Invalid animate!")
+            }
+            
+            self.stageAnimation += 1
+        }
     }
 }

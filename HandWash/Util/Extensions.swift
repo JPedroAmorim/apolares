7a70474c0aeb76557e26ea28d3739c3f46f71e1f
João Pedro <joaopdamorim@gmail.com>
2020-05-14 09:36:56 -0300

    String extension

---
+++ HandWash/Util/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  HandWash WatchKit Extension
+//
+//  Created by João Pedro de Amorim on 14/05/20.
+//  Copyright © 2020 AndrePapoti. All rights reserved.
+//
+
+import Foundation

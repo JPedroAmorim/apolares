69995c71300b37553a5062ad69dc2aa5da3f815f
João Pedro <joaopdamorim@gmail.com>
2020-05-20 11:13:11 -0300

    building but crashing

---
+++ HandWashApp/Other/DateUtil.swift
@@ -0,0 +1,23 @@
+//
+//  DateUtil.swift
+//  HandWashApp WatchKit Extension
+//
+//  Created by João Pedro de Amorim on 20/05/20.
+//  Copyright © 2020 AndrePapoti. All rights reserved.
+//
+
+
+import Foundation
+
+class DateUtil {
+    static let shared = DateUtil()
+    
+    private init () {
+        self.formatter.dateFormat = "dd/MM/yyyy"
+        self.formatter.timeStyle = .none
+        self.formatter.dateStyle = .short
+    }
+    
+    var formatter = DateFormatter()
+    
+}
---
+++ HandWashApp/Other/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  HandWashApp WatchKit Extension
+//
+//  Created by João Pedro de Amorim on 20/05/20.
+//  Copyright © 2020 AndrePapoti. All rights reserved.
+//
+
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
